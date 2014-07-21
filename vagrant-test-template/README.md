da9bddeddae53676b1341b0ec95c6b9faafaf837
Michele Paolino <m.paolino@virtualopensystems.com>
2014-07-21 19:10:49 +0200

    Fix vagrant-test template fix
    Semplification and fix of vagrant-test template. To run a VM
    and automatically install devstack and execute tempest, execute:
    
        $ cp local.conf.template local.conf
        $ ./runtest.sh
    
    The support for devstack and the installation of libvirt and qemu
    from git repositories is present.
    
    Signed-off-by: Michele Paolino <m.paolino@virtualopensystems.com>

---
+++ vagrant-test-template/README.md
@@ -0,0 +1,13 @@
+# shellci
+
+### Vagrant test template
+
+Vagrant test template shows how to create a new throw-away VM, execute dev-stack and finally run tempest tests.
+You can customize the dev-stack and the tempest tests modifying the files local.conf.template and inside-vagrant.sh.
+
+To test it, just run:
+```
+cp local.conf.template local.conf
+./runtest.sh
+```
+
--- vagrant-test-template/inside_vagrant.sh
+++ vagrant-test-template/inside_vagrant.sh
@@ -11,13 +11,13 @@ echo "ok"             >> /vagrant/progress.txt
 # Install and run devstack
 echo -n "$(date '+%F %T') devstack.. " >> /vagrant/progress.txt
 cd $HOME
-git clone https://github.com/openstack-dev/devstack.git
+git clone https://github.com/virtualopensystems/devstack.git
 cd devstack
 cp /vagrant/local.conf .
 ./stack.sh
 echo "ok" >> /vagrant/progress.txt
 
-# Install and run tempest                                                                                                             
+# Install and run tempest
 echo -n "$(date '+%F %T') tempest.. " >> /vagrant/progress.txt
 #cd $HOME
 #git clone git://git.openstack.org/openstack/tempest.git
@@ -25,12 +25,14 @@ echo -n "$(date '+%F %T') tempest.. " >> /vagrant/progress.txt
 #sudo python ./setup.py install
 cd /opt/stack/tempest
 testr init
-testr run tempest.api.network.test_networks | tee /vagrant/tempest.log
+testr run tempest.api.network | tee -a /vagrant/tempest.log
+testr run tempest.scenario.test_network_advanced_server_ops | tee -a /vagrant/tempest.log
+testr run tempest.scenario.test_network_basic_ops | tee -a /vagrant/tempest.log
 if grep PASSED /vagrant/tempest.log; then
-    echo "passed" >> /vagrant/progress.txt
+    echo "SUCCESS" >> /vagrant/progress.txt
 else
-    echo "failed" >> /vagrant/progress.txt
+    echo "FAILURE" >> /vagrant/progress.txt
 fi
 
-# That's it                                                                                                                           
+# That's it
 echo "$(date '+%F %T') Finished" >> /vagrant/progress.txt
